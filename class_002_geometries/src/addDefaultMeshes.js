2145977bfccb0db3362d63a418e7e1b1ed0852ff
Chioma Obaji <chioma@Chiomas-MacBook-Air.local>
2026-02-17 23:01:50 +0100

    made not bad lol thx brian

--- class_002_geometries/src/addDefaultMeshes.js
+++ class_002_geometries/src/addDefaultMeshes.js
@@ -8,12 +8,3 @@ const mesh = new THREE.Mesh(geometry,material);
 return mesh;
 
 }
-
-export function addStandardMeshes(){
-    const geometry = new THREE.BoxGeometry(1, 1, 1)
-    const material = new THREE.MeshStandardMaterial({
-        color: 0x00ff00
-    })
-    const mesh = new THREE.Mesh(geometry, material)
-    return mesh
-}
--- class_002_geometries/src/main.js
+++ class_002_geometries/src/main.js
@@ -1,7 +1,8 @@
 import './style.css'
 import * as THREE from 'three' 
-import {addDefaultMeshes, addStandardMeshes } from './addDefaultMeshes.js'
-import { addLight } from '../addLight.js';
+import { addDefaultMeshes } from './addDefaultMeshes.js'
+import { addSphereMesh } from './addSphereMesh.js'
+import { addTorusMesh } from './addTorusMesh.js'
 
 const scene = new THREE.Scene();
 // (FOV, ASPECT, RATIO, NEAR, FAR)
@@ -9,7 +10,7 @@ const camera = new THREE.PerspectiveCamera(75, window.innerWidth/ window.innerHe
 const renderer = new THREE.WebGLRenderer({antialias: true});
 
 const meshes = {}
-const lights = {}
+let tick = 0 
 
 
 init()
@@ -19,37 +20,43 @@ function init() {
   document.body.appendChild(renderer.domElement);
 camera.position.z= 5
 
-lights.default = addLight()
-scene.add(lights.default)
-
 //where we populate mesh container "filling cabnet of objects"
 meshes.default = addDefaultMeshes();
 meshes.default.position.x = 2
 
-meshes.standard = addStandardMeshes()
-meshes.standard.position.x = -2
+meshes.default2 = addDefaultMeshes();
+meshes.default2.position.x = -2
+
+meshes.default3 = addDefaultMeshes();
+meshes.default3.position.y = 2
+
+meshes.sphere = addSphereMesh()
+meshes.torus = addTorusMesh()
+
 
 //adds meshes to our scene
 scene.add(meshes.default);
-scene.add(meshes.standard);
+scene.add(meshes.default2);
+scene.add(meshes.default3);
+scene.add(meshes.sphere)
+scene.add(meshes.torus)
 
 
-
-
-//  console.log(meshes)
+ console.log(meshes)
 // console.log(meshes.test)
 
-resize()
   animate()
-}
-function resize (){
-
 }
 
 //ROOOOOTATIONS
 function animate(){
   requestAnimationFrame(animate);
   renderer.render(scene, camera)
+  tick += 0.01
   meshes.default.rotation.x +=0.01
-  meshes.standard.rotation.y +=0.01
+   meshes.default2.rotation.y -=0.1
+   meshes.default3.rotation.z +=0.1
+   meshes.torus.rotation.x += 0.01
+   meshes.sphere.position.x = Math.sin(tick * 5) * 4
+  meshes.sphere.position.y = Math.cos(tick * 5) * 4
 }
